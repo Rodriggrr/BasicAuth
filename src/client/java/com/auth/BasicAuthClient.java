09f931b1f2faae39212e87d9d490818b4382e8c8
Rodrigo Farinon <rfarinon@alu.ufc.br>
2025-07-15 00:07:58 -0300

    1.21.7 init

---
+++ src/client/java/com/auth/BasicAuthClient.java
@@ -0,0 +1,10 @@
+package com.auth;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BasicAuthClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
